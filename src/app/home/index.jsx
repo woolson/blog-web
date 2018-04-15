4bb0e92766e756749e59676c3a1499edb747b98c
woolson <woolson.lee@gmail.com>
2018-04-15 19:54:05 +0800

    修改主页内容

--- src/app/home/index.jsx
+++ src/app/home/index.jsx
@@ -2,19 +2,25 @@
 * @Author: woolson
 * @Date:   2017-06-24 12:57:20
 * @Email:   woolson.lee@gmail.com
-* @Last Modified by:   woolson
-* @Last Modified time: 2017-07-01 11:39:19
+ * @Last Modified by: woolson
+ * @Last Modified time: 2018-04-15 19:45:01
 */
 
 import "./style"
 import axios from "axios"
 import React from "react"
+import { Link } from 'react-router-dom'
 import Banner from "Static/images/banner.jpg"
 import Update from "Static/faker/update"
 
 export default class Home extends React.Component {
   state = {
     update: [],
+    tags: [
+      { name: "前端", path: "frontend" },
+      { name: "笔记", path: "note" },
+      { name: "BUG", path: "bug" },
+    ],
   }
 
   componentDidMount() {
@@ -37,15 +43,23 @@ export default class Home extends React.Component {
     })
   }
 
+  renderTags() {
+    return this.state.tags.map(o => {
+      const path = `/study/${o.path}`
+      return <Link to={path}>{o.name}</Link>
+    })
+  }
+
   render() {
     return (
       <div className="home">
         <div className="home-block">
-          <div className="home-block__title">最近更新</div>
+          { this.renderTags() }
+          {/* <div className="home-block__title">最近更新</div> */}
 
-          <div className="home-block__content">
+          {/* <div className="home-block__content">
             { this.renderItems() }
-          </div>
+          </div> */}
         </div>
       </div>
     )
